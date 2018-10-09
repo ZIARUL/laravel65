99cc2732c70ba4945b2d666353aefa0f3408b795
ziarul92 <ziarul92@gmail.com>
2018-10-09 23:46:57 +0600

    first commit

--- resources/views/front/about/about-content.blade.php
+++ resources/views/front/about/about-content.blade.php
@@ -3,7 +3,14 @@
   About section
  @endsection
 
- @section('content')
-   sadlkfjsa dlfaslkf asldkf asldkfjsadlkf sadlkfj sadlkfjsadlkfasdlkf asdlkfa 
 
- @endsection
+	 @section('content')
+
+
+	  <div style="margin-top:50px;">
+	  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorem dolorum nesciunt veniam fugit quam ex. Saepe consequuntur doloribus distinctio eveniet quis fugit voluptatem, aperiam tempore autem, iste, est voluptate?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad dignissimos rerum laborum. Soluta deserunt possimus in tenetur nihil deleniti? A sunt perspiciatis quisquam ea doloribus, ipsum vitae ullam cumque!</p>
+	  </div>
+	  <a href="#" style="margin-bottom: 50px; display: inline-block;">More</a>
+	 @endsection
+
+
